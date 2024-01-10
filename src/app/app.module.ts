c331c6a54dd8c36e0c72b437cee0326e28b418ad
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2024-01-10 16:40:46 -0300

    title and button fixed in new contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {LoginModule} from "./pages/login/login.module";
 import {ReactiveFormsModule} from "@angular/forms";
-import {SignupModule} from "./pages/signup/signup.module";
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from "./core/core.module";
 import {PagesModule} from "./pages/pages.module";
--- src/app/shared/search-contact/search-contact.component.ts
+++ src/app/shared/search-contact/search-contact.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ContactService} from "../../core/service/contact/contact.service";
 import {DataService} from "../../core/utils/data.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'search-contact',
@@ -11,9 +12,13 @@ export class SearchContactComponent implements OnInit {
   @Output() searchValue: EventEmitter<String> = new EventEmitter<String>();
 
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  newContactButton(){
+    this.router.navigate(['newContact']);
   }
 
   search(value: String) {
